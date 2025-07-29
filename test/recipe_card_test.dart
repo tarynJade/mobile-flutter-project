fe03b198cf8aa5b88e3cda5aabfc559982aec04f
tarynJade <taryn.bilsborough@moneysupermarket.com>
2025-07-29 14:53:19 +0100

    test: Add tests for RecipeCard to validate title display, image absence, and long title handling

--- test/recipe_card_test.dart
+++ test/recipe_card_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:ada_flutter_project/models/recipe.dart';
 import 'package:ada_flutter_project/widgets/recipe_card.dart';
 
-
 void main() {
   testWidgets('RecipeCard displays recipe title', (WidgetTester tester) async {
     final recipe = Recipe(id: 1, title: 'Pasta', image: '', ingredients: [], analyzedInstructions: []);
@@ -14,5 +13,40 @@ void main() {
     );
 
     expect(find.text('Pasta'), findsOneWidget);
+  });
+
+  testWidgets('RecipeCard does not display image if image URL is empty', (WidgetTester tester) async {
+    final recipe = Recipe(
+      id: 3,
+      title: 'Soup',
+      image: '',
+      ingredients: [],
+      analyzedInstructions: [],
+    );
+    await tester.pumpWidget(
+      MaterialApp(
+        home: RecipeCard(recipe: recipe),
+      ),
+    );
+
+    expect(find.byType(Image), findsNothing);
+  });
+
+  testWidgets('RecipeCard handles long titles with ellipsis', (WidgetTester tester) async {
+    final longTitle = 'A very long recipe title that should be truncated with an ellipsis at the end';
+    final recipe = Recipe(
+      id: 4,
+      title: longTitle,
+      image: '',
+      ingredients: [],
+      analyzedInstructions: [],
+    );
+    await tester.pumpWidget(
+      MaterialApp(
+        home: RecipeCard(recipe: recipe),
+      ),
+    );
+
+    expect(find.textContaining('A very long recipe title'), findsOneWidget);
   });
 }
